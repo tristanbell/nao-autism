fa79dc64090e25e7336068542ae7835c775b9594
AlexMaley <alexmaley@hotmail.com>
2013-07-31 15:09:07 +0100

    Started to code the basic structure of the guessing game.

--- src/emotion_game/include/GuessGame.h
+++ src/emotion_game/include/GuessGame.h
@@ -8,14 +8,46 @@
 #ifndef GUESSGAME_H_
 #define GUESSGAME_H_
 
+#include <ros/ros.h>
+
+#include <nao_msgs/WordRecognized.h>
+
+#include <string>
+#include <list>
+
 class GuessGame : public Game {
 
 public:
+	GuessGame(GameSettings& settings);
+
 	void perform(void);
 
 protected:
 	void startGame(void);
 	void endGame(void);
+
+private:
+	ros::NodeHandle _guessNodeHandle;
+	ros::Subscriber _speechSubscriber;
+
+	bool _waitingSpeech;
+	int currentWait;
+
+	std::list<std::pair<std::string, float> > _recognizedWords;
+
+	//State information related variables/structures
+	enum State
+	{
+		INTRODUCTION,
+		PERFORM_EMOTION,
+		WAITING_ANSWER_QUESTION,
+		WAITING_ANSWER_CONTINUE,
+	};
+
+	State _currentState;
+	bool _performedEmotion;
+
+	void onSpeechRecognized(const nao_msgs::WordRecognized msg);
 
 };
 
--- src/emotion_game/src/GuessGame.cpp
+++ src/emotion_game/src/GuessGame.cpp
@@ -8,16 +8,79 @@
 #include <Game.h>
 #include <GuessGame.h>
 
-void GuessGame::startGame(void) {
+#define WORD_RECOGNIZED_TOPIC "word_recognized"
 
+#include <iostream>
+
+GuessGame::GuessGame(GameSettings& settings) :  Game(settings),
+											   _guessNodeHandle(),
+											   _waitingSpeech(false),
+											   _recognizedWords(),
+											   _currentState(INTRODUCTION),
+											   _performedEmotion(false)
+{
+	_speechSubscriber = _guessNodeHandle.subscribe("word_recognized", 1000, &GuessGame::onSpeechRecognized, &*this);
+}
+
+void GuessGame::startGame(void) {
+	isDone = false;
+	_waitingSpeech = false;
+
+	//Reset state information
+	_currentState = INTRODUCTION;
+	_performedEmotion = false;
 }
 
 void GuessGame::perform(void) {
+	switch (_currentState){
 
+	case INTRODUCTION:{
+		//Perform introduction speeches/actions, etc
+		break;
+	}
+
+	case PERFORM_EMOTION:{
+		//Perform emotion and then ask the child a question
+		if (_performedEmotion){
+
+		}else{
+			_performedEmotion = true;
+		}
+
+		break;
+	}
+
+	case WAITING_ANSWER_QUESTION:{
+		//Wait until answer is given
+
+		break;
+	}
+
+	case WAITING_ANSWER_CONTINUE:{
+		//Wait until yes/no is 'heard'
+
+		break;
+	}
+
+	}
 }
 
 void GuessGame::endGame(void) {
+	_waitingSpeech = false;
+}
 
+void GuessGame::onSpeechRecognized(const nao_msgs::WordRecognized msg)
+{
+	//Check to see if speech is needed, if so push onto list
+	if (!isDone && _waitingSpeech){
+		for (int i=0;i<msg.words.size();i++){
+			std::pair<std::string, float> pair(msg.words[i], msg.confidence_values[i]);
+
+			std::cout << "Recognized word: " << msg.words[i] << ", confidence: " << msg.confidence_values[i] << "\n";
+
+			_recognizedWords.push_back(pair);
+		}
+	}
 }
 
 
